ccf16a5ea5235d6a80bf52001a5a08cba30f4450
Asalle <asalle.kim@gmail.com>
2017-10-28 19:54:50 +0200

    Add iterator

--- 2/src/Deque.java
+++ 2/src/Deque.java
@@ -5,7 +5,7 @@ import java.util.NoSuchElementException;
  * Created by mirzaiev on 27.10.2017.
  */
 
-public class Deque<Item> {/* implements Iterable<Item> { */
+public class Deque<Item> implements Iterable<Item> {
 
     private class Node {
         private Node next;
@@ -20,19 +20,27 @@ public class Deque<Item> {/* implements Iterable<Item> { */
         }
     }
 
-//    private class DequeIterator implements Iterator<Item> {
-//        private Node cur;
-//        DequeIterator() {
-//
-//        }
-//
-//        public boolean hasNext() {
-//
-//        }
-//
-//        public Item next() {
-//        }
-//    }
+    private class DequeIterator implements Iterator<Item> {
+        private Node cur;
+        DequeIterator() {
+            cur = head;
+        }
+
+        public boolean hasNext() {
+            return cur != null && cur.next != null;
+        }
+
+        public Item next() {
+            if (cur != null) {
+                Item now = cur.val;
+                cur = cur.next;
+                return now;
+            }
+            else {
+                throw new NoSuchElementException();
+            }
+        }
+    }
 
     private Node head;
     private Node tail;
@@ -55,6 +63,8 @@ public class Deque<Item> {/* implements Iterable<Item> { */
 
         Node temp = new Node(head, null, item);
         head = temp;
+        if (tail == null)
+            tail = temp;
         size++;
     }         // add the item to the front
 
@@ -66,25 +76,44 @@ public class Deque<Item> {/* implements Iterable<Item> { */
         Node temp = new Node(null, tail, item);
         tail = temp;
         size++;
+        if (head == null)
+            head = temp;
     }           // add the item to the end
 
     public Item removeFirst() {
         if (isEmpty()) {
             throw new NoSuchElementException();
         }
+
         Item temp = head.val;
+        if (head == tail)
+        {
+            tail = null;
+        }
+
+        head = head.next;
+        size--;
         return  temp;
     }                // remove and return the item from the front
 
-//    public Item removeLast() {
-//        if (isEmpty()) {
-//            throw new NoSuchElementException();
-//        }
-//    }                 // remove and return the item from the end
+    public Item removeLast() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
 
-//    public Iterator<Item> iterator() {
-//
-//    }         // return an iterator over items in order from front to end
+        Item temp = tail.val;
+        if (head == tail) {
+            head = null;
+        }
+        tail = tail.prev;
+        size--;
+
+        return temp;
+    }                 // remove and return the item from the end
+
+    public Iterator<Item> iterator() {
+        return new DequeIterator();
+    }         // return an iterator over items in order from front to end
 
     public static void main(String[] args)   // unit testing (optional)
     {
--- 2/test/DequeTest.java
+++ 2/test/DequeTest.java
@@ -1,5 +1,10 @@
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
 import static org.testng.Assert.*;
 
 /**
@@ -51,6 +56,66 @@ public class DequeTest {
         String item = "Hello World!";
         deq.addLast(item);
         deq.removeFirst();
+        assertEquals(deq.isEmpty(), true);
+        assertEquals(deq.size(), 0);
     }
 
+    @Test
+    public void testRemoveLast() throws Exception {
+        Deque<String> deq = new Deque<String>();
+        assertEquals(deq.isEmpty(), true);
+        assertEquals(deq.size(), 0);
+
+        String item = "Hello World!";
+        deq.addFirst(item);
+        deq.removeLast();
+        assertEquals(deq.isEmpty(), true);
+        assertEquals(deq.size(), 0);
+    }
+
+    @Test
+    public void testIteratorManual() throws Exception {
+        Deque<String> deq = new Deque<String>();
+        deq.addFirst("Hello");
+        deq.addFirst("world");
+        assertEquals(deq.size(), 2);
+
+        Iterator<String> it = deq.iterator();
+
+        assertEquals(it.next(), "world");
+        assertEquals(it.next(), "Hello");
+    }
+
+    @Test
+    public void testIteratorForeach() throws Exception {
+        Deque<String> deq = new Deque<String>();
+        List<String> testList = new ArrayList<String>();
+        testList.add("Hello");
+        testList.add("World");
+
+        for (String item: testList) {
+            deq.addLast(item);
+        }
+        assertEquals(deq.size(), 2);
+
+        int i = 0;
+        for (String item: deq) {
+            assertEquals(item, testList.get(i));
+            i++;
+        }
+    }
+
+    @Test(expectedExceptions = NoSuchElementException.class)
+    public void testIteratorOutOfBound() throws Exception {
+        Deque<String> deq = new Deque<String>();
+        deq.addFirst("Hello");
+        deq.addFirst("world");
+        assertEquals(deq.size(), 2);
+
+        Iterator<String> it = deq.iterator();
+
+        assertEquals(it.next(), "world");
+        assertEquals(it.next(), "Hello");
+        it.next();
+    }
 }
