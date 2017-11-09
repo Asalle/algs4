1df6f938a89f04bbfe90612bb380f82749015bc6
Asalle <asalle.kim@gmail.com>
2017-11-09 08:43:55 +0100

    Preserve 4 for a while

---
+++ 4/test/BoardTest.java
@@ -0,0 +1,43 @@
+import org.testng.annotations.Test;
+
+/**
+ * Created by mirzaiev on 6.11.2017.
+ */
+public class BoardTest {
+    @Test
+    public void testDimension() throws Exception {
+    }
+
+    @Test
+    public void testHamming() throws Exception {
+    }
+
+    @Test
+    public void testManhattan() throws Exception {
+    }
+
+    @Test
+    public void testIsGoal() throws Exception {
+    }
+
+    @Test
+    public void testTwin() throws Exception {
+    }
+
+    @Test
+    public void testEquals() throws Exception {
+    }
+
+    @Test
+    public void testNeighbors() throws Exception {
+    }
+
+    @Test
+    public void testToString() throws Exception {
+    }
+
+    @Test
+    public void testMain() throws Exception {
+    }
+
+}
