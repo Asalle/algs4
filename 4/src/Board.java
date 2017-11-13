50e93fbf46c125838d83c6cc2e47ac34b0501895
Asalle <asalle.kim@gmail.com>
2017-11-13 14:12:59 +0100

    Add some basic Board methods

--- 4/src/Board.java
+++ 4/src/Board.java
@@ -1,16 +1,31 @@
-//
-//
-//public class Board {
-//    private final Integer dimension;
-//    public Board(int[][] blocks) {
-//        dimension = blocks.length;
-//    }          // construct a board from an n-by-n array of blocks
-//               // (where blocks[i][j] = block in row i, column j)
-//
-//    public int dimension() {
-//        return dimension;
-//    }                 // board dimension n
-//
+public class Board {
+    private Integer dimension;
+    private Integer[][] board;
+
+    private int[][] generateGoal(int n) {
+        int[][] tempBoard = new int[n][n];
+        int i = 0;
+        while (i < n*n-1) {
+            tempBoard[i/n][i%n] = i+1;
+            i++;
+        }
+        return tempBoard;
+    }
+
+    public Board(int[][] blocks) {
+        dimension = blocks.length;
+        board = new Integer[dimension][dimension];
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                board[i][j] = blocks[i][j];
+            }
+        }
+    }
+
+    public int dimension() {
+        return dimension;
+    }
+
 //    public int hamming() {
 //
 //    }                  // number of blocks out of place
@@ -19,28 +34,54 @@
 //
 //    }                 // sum of Manhattan distances between blocks and goal
 //
-//    public boolean isGoal() {
-//
-//    }                // is this board the goal board?
+    public boolean isGoal() {
+        return this.equals(new Board(generateGoal(dimension)));
+    }
 //
 //    public Board twin() {
 //
 //    }                   // a board that is obtained by exchanging any pair of blocks
 //
-//    public boolean equals(Object y) {
-//
-//    }       // does this board equal y?
-//
+    public boolean equals(Object y) {
+        if (this == y)
+            return true;
+
+        Board that = (Board) y;
+        if (this.dimension() != that.dimension()) {
+            return false;
+        }
+
+        int size = this.dimension();
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (this.board[i][j] != that.board[i][j])
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
 //    public Iterable<Board> neighbors() {
 //
 //    }     // all neighboring boards
 //
-//    public String toString() {
-//
-//    }               // string representation of this board (in the output format specified below)
-//
-//    public static void main(String[] args) {
-//
-//    }// unit tests (not graded)
-//}
-//
+    public String toString() {
+        StringBuffer boardString = new StringBuffer();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                boardString.append(board[i][j] + " ");
+            }
+            boardString.append("\n");
+        }
+
+        return boardString.toString();
+    }
+
+    public static void main(String[] args) {
+        int[][] a = new int[4][4];
+        Board b = new Board(a);
+        System.out.println(b.toString());
+    }
+}
+
--- 4/test/BoardTest.java
+++ 4/test/BoardTest.java
@@ -6,6 +6,16 @@ import org.testng.annotations.Test;
 public class BoardTest {
     @Test
     public void testDimension() throws Exception {
+        int size = 20;
+        int[][] b = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                b[i][j] = 12;
+            }
+        }
+        Board board = new Board(b);
+
+        assert board.dimension() == b.length;
     }
 
     @Test
@@ -18,6 +28,26 @@ public class BoardTest {
 
     @Test
     public void testIsGoal() throws Exception {
+        int size = 5;
+        int[][] b = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                b[i][j] = 12;
+            }
+        }
+        Board board = new Board(b);
+
+        assert board.isGoal() == false;
+
+        int[][] tempBoard = new int[size][size];
+        int i = 0;
+        while (i < size*size-1) {
+            tempBoard[i/size][i%size] = i+1;
+            i++;
+        }
+
+        Board goalBoard = new Board(tempBoard);
+        assert goalBoard.isGoal() == true;
     }
 
     @Test
@@ -26,6 +56,26 @@ public class BoardTest {
 
     @Test
     public void testEquals() throws Exception {
+        final int size = 5;
+        int[][] boardContent = new int[size][size];
+
+        int k = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                boardContent[i][j] = k+1;
+                k++;
+            }
+        }
+
+        Board a = new Board(boardContent);
+        Board b = new Board(boardContent);
+
+        assert a.equals(b) == true;
+
+        boardContent[0][0] = 101;
+        Board c = new Board(boardContent);
+
+        assert a.equals(c) == false;
     }
 
     @Test
