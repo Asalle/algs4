dc27f2f5afd64ca52a1d00124e034b09b1857c81
Asalle <asalle.kim@gmail.com>
2017-11-15 12:45:35 +0100

    5: Add PointSET

---
+++ 5/test/PointSETTest.java
@@ -0,0 +1,32 @@
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class PointSETTest {
+    @Test
+    public void testIsEmpty() throws Exception {
+        PointSET set = new PointSET();
+        assertEquals(set.isEmpty(), true);
+    }
+
+    @Test
+    public void testSize() throws Exception {
+    }
+
+    @Test
+    public void testInsert() throws Exception {
+    }
+
+    @Test
+    public void testContains() throws Exception {
+    }
+
+    @Test
+    public void testRange() throws Exception {
+    }
+
+    @Test
+    public void testNearest() throws Exception {
+    }
+
+}
