02c205ebaf173e38617603f733c4aa01b4f755f0
Asalle <asalle.kim@gmail.com>
2017-11-15 15:30:26 +0100

    5: Add PointSet

--- 5/src/PointSET.java
+++ 5/src/PointSET.java
@@ -1,38 +1,105 @@
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.SET;
+import edu.princeton.cs.algs4.StdDraw;
 
 import java.util.ArrayList;
 
 public class PointSET {
     private SET<Point2D> pointSet;
+    private double maxX;
+    private double maxY;
 
     public PointSET() {
+        pointSet = new SET<>();
+        maxX = 0;
+        maxY = 0;
+    }
 
-    }                            // construct an empty set of points
     public boolean isEmpty() {
-        return false;
-    }                      // is the set empty?
+        return pointSet.isEmpty();
+    }
+
     public int size() {
-        return 12;
-    }                         // number of points in the set
+        return pointSet.size();
+    }
+
     public void insert(Point2D p) {
+        if (p == null) {
+            throw new IllegalArgumentException();
+        }
 
-    }              // add the point to the set (if it is not already in the set)
+        if (!pointSet.contains(p)) {
+            pointSet.add(p);
+
+            if (p.x() > maxX)
+                maxX = p.x();
+
+            if (p.y() > maxY)
+                maxY = p.y();
+        }
+    }
+
     public boolean contains(Point2D p) {
-        return  false;
-    }            // does the set contain point p?
-    public void draw() {
+        if (p == null) {
+            throw new IllegalArgumentException();
+        }
 
-    }                         // draw all points to standard draw
+        return pointSet.contains(p);
+    }
+
+    public void draw() {
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(0, maxX+1);
+        StdDraw.setYscale(0, maxY+1);
+        for (Point2D p : pointSet) {
+            p.draw();
+        }
+        StdDraw.show();
+    }
+
     public Iterable<Point2D> range(RectHV rect) {
-        return new ArrayList<Point2D>();
-    }             // all points that are inside the rectangle (or on the boundary)
+        if (rect == null) {
+            throw new IllegalArgumentException();
+        }
+
+        ArrayList<Point2D> insideList = new ArrayList<>();
+
+        for (Point2D point: pointSet) {
+            if(rect.contains(point)) {
+                insideList.add(point);
+            }
+        }
+
+        return insideList;
+    }
     public Point2D nearest(Point2D p) {
-        return new Point2D(2, 3);
-    }             // a nearest neighbor in the set to point p; null if the set is empty
+        if (p == null) {
+            throw new IllegalArgumentException();
+        }
+
+        if (pointSet.isEmpty()) {
+            return null;
+        }
+
+        Point2D minDistancePoint = new Point2D(Double.MAX_VALUE, Double.MAX_VALUE);
+        for (Point2D point: pointSet) {
+            double currentDistance = p.distanceTo(point);
+            double minDistance = p.distanceSquaredTo(minDistancePoint);
+            if (Double.compare(minDistance, currentDistance) > 0) {
+                minDistancePoint = point;
+            }
+        }
+
+        return minDistancePoint;
+    }
 
     public static void main(String[] args) {
+        PointSET set = new PointSET();
+        set.insert(new Point2D(2, 3));
+        set.insert(new Point2D(5, 6));
+        set.insert(new Point2D(1, 10));
 
-    }                 // unit testing of the methods (optional)
+        set.draw();
+    }
 }
--- 5/test/PointSETTest.java
+++ 5/test/PointSETTest.java
@@ -1,3 +1,4 @@
+import edu.princeton.cs.algs4.Point2D;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
@@ -7,18 +8,37 @@ public class PointSETTest {
     public void testIsEmpty() throws Exception {
         PointSET set = new PointSET();
         assertEquals(set.isEmpty(), true);
+
+        set.insert(new Point2D(2, 4));
+        assertEquals(set.isEmpty(), false);
     }
 
     @Test
     public void testSize() throws Exception {
+        PointSET set = new PointSET();
+        assertEquals(set.size(), 0);
+
+        set.insert(new Point2D(1, 2));
+        assertEquals(set.size(), 1);
     }
 
     @Test
     public void testInsert() throws Exception {
+        PointSET set = new PointSET();
+        Point2D point2D = new Point2D(1, 2);
+        set.insert(point2D);
+        assertEquals(set.contains(point2D), true);
     }
 
     @Test
     public void testContains() throws Exception {
+        PointSET set = new PointSET();
+        Point2D point2D = new Point2D(1, 2);
+        set.insert(point2D);
+        assertEquals(set.contains(point2D), true);
+
+        Point2D notInSet = new Point2D(32, 2);
+        assertEquals(set.contains(notInSet), false);
     }
 
     @Test
