5c28555292b5cff0d73290befccc5b267ca018ab
Asalle <asalle.kim@gmail.com>
2017-11-16 09:28:09 +0100

    5: Add KdTree

---
+++ 5/test/KdTreeTest.java
@@ -0,0 +1,28 @@
+import org.testng.annotations.Test;
+
+public class KdTreeTest {
+    @Test
+    public void testIsEmpty() throws Exception {
+    }
+
+    @Test
+    public void testSize() throws Exception {
+    }
+
+    @Test
+    public void testInsert() throws Exception {
+    }
+
+    @Test
+    public void testContains() throws Exception {
+    }
+
+    @Test
+    public void testRange() throws Exception {
+    }
+
+    @Test
+    public void testNearest() throws Exception {
+    }
+
+}
