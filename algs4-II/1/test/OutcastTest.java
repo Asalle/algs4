bd6c85684d111776223e93b125a36d6c907405d6
Asalle <asalle.kim@gmail.com>
2017-11-21 11:33:06 +0100

    1: Add outcast

---
+++ algs4-II/1/test/OutcastTest.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by mirzaiev on 21.11.2017.
+ */
+public class OutcastTest {
+    @Test
+    public void testOutcast() throws Exception {
+    }
+
+}
