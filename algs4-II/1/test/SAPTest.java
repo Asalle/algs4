ab1a82d5ad4726ed25793730babdb6f026f9c3b2
Asalle <asalle.kim@gmail.com>
2017-11-21 11:30:28 +0100

    1: Add SAP and tests

---
+++ algs4-II/1/test/SAPTest.java
@@ -0,0 +1,25 @@
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by mirzaiev on 21.11.2017.
+ */
+public class SAPTest {
+    @Test
+    public void testLength() throws Exception {
+    }
+
+    @Test
+    public void testAncestor() throws Exception {
+    }
+
+    @Test
+    public void testLength1() throws Exception {
+    }
+
+    @Test
+    public void testAncestor1() throws Exception {
+    }
+
+}
