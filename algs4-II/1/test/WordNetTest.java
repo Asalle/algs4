c236f2fe975a1c52edd3db4a559cdbffebe76155
Asalle <asalle.kim@gmail.com>
2017-11-21 11:01:15 +0100

    Add algs4.jar to II

---
+++ algs4-II/1/test/WordNetTest.java
@@ -0,0 +1,25 @@
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by mirzaiev on 21.11.2017.
+ */
+public class WordNetTest {
+    @Test
+    public void testNouns() throws Exception {
+    }
+
+    @Test
+    public void testIsNoun() throws Exception {
+    }
+
+    @Test
+    public void testDistance() throws Exception {
+    }
+
+    @Test
+    public void testSap() throws Exception {
+    }
+
+}
